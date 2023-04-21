cea001025f0da43c67076b97c91fed02b01082b7
Egor Yasinovskiy <yasinovskiy.egor1@gmail.com>
2023-04-21 18:41:16 +0300

    Команда выписки из санатория

---
+++ Sanatorium.PatientService/CQRS/Commands/Discharge/Discharge.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Sanatorium.PatientService.CQRS.Commands.RegisterNew.Discharge
+{
+    public class Discharge : IRequest
+    {
+        public Guid ID { get; set; }
+    }
+}
---
+++ Sanatorium.PatientService/CQRS/Commands/Discharge/DischargeHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+
+namespace Sanatorium.PatientService.CQRS.Commands.RegisterNew.Discharge
+{
+    public class DischargeHandler : IRequestHandler<Discharge>
+    {
+        IPatientRepository _patientRepository;
+
+        public DischargeHandler(IPatientRepository patientRepository)
+        {
+            _patientRepository = patientRepository;
+        }
+
+        public async Task Handle(Discharge request, CancellationToken cancellationToken)
+        {
+            var patient = await _patientRepository.GetById(request.ID, cancellationToken);
+            if (patient != null)
+            {
+                patient.Discharged = true;
+                patient.DateRegistered = DateOnly.FromDateTime(DateTime.Now);
+                await _patientRepository.Update(patient, cancellationToken);
+                await _patientRepository.SaveChanges(cancellationToken);
+            }
+        }
+    }
+}
