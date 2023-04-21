b5572b3c074301e7c06b271efcc3c9868dbf8038
Egor Yasinovskiy <yasinovskiy.egor1@gmail.com>
2023-04-21 18:41:02 +0300

    Обработчики команд регистрации в системе и приема в санаторий

--- Sanatorium.PatientService/CQRS/Commands/Register/RegisterHadler.cs
+++ Sanatorium.PatientService/CQRS/Commands/Register/RegisterHadler.cs
@@ -1,6 +1,26 @@
-﻿namespace Sanatorium.PatientService.CQRS.Commands.Register
+﻿using MediatR;
+
+namespace Sanatorium.PatientService.CQRS.Commands.Register
 {
-	public class RegisterHadler
+	public class RegisterHadler : IRequestHandler<Register>
 	{
+		IPatientRepository _patientRepository;
+
+		public RegisterHadler(IPatientRepository patientRepository)
+		{
+			_patientRepository = patientRepository;
+		}
+
+		public async Task Handle(Register request, CancellationToken cancellationToken)
+		{
+			var patient = await _patientRepository.GetById(request.Id, cancellationToken);
+			if (patient != null)
+			{
+				patient.Discharged = false;
+				patient.DateRegistered = DateOnly.FromDateTime(DateTime.Now);
+				await _patientRepository.Update(patient, cancellationToken);
+				await _patientRepository.SaveChanges(cancellationToken);
+			}
+		}
 	}
 }
--- Sanatorium.PatientService/CQRS/Commands/RegisterNew/RegisterNewHandler.cs
+++ Sanatorium.PatientService/CQRS/Commands/RegisterNew/RegisterNewHandler.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
+
+using Sanatorium.PatientService.DataModel;
 
 namespace Sanatorium.PatientService.CQRS.Commands.RegisterNew
 {
-	public class RegisterNewHandler
+	public class RegisterNewHandler : IRequestHandler<RegisterNew>
 	{
+		IPatientRepository _patientRepository;
+
+		public async Task Handle(RegisterNew request, CancellationToken cancellationToken)
+		{
+			var patient = new Patient();
+			patient.BirthDate = request.BirthDate;
+			patient.PhoneNumber = request.PhoneNumber;
+			patient.FirstName = request.FirstName;
+			patient.LastName = request.LastName;
+			patient.MiddleName= request.MiddleName;
+			patient.DateRegistered = DateOnly.FromDateTime(DateTime.Now);
+			patient.Id = Guid.NewGuid();
+
+			await _patientRepository.Create(patient, cancellationToken);
+			await _patientRepository.SaveChanges(cancellationToken);
+		}
 	}
 }
