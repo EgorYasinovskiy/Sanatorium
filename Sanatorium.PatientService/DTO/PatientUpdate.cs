f0464af81a4a3da05a2e8e2ac53d7b95b2911fe6
Egor Yasinovskiy <yasinovskiy.egor1@gmail.com>
2023-04-25 20:23:01 +0300

    забыл добавить DTO в коммит

---
+++ Sanatorium.PatientService/DTO/PatientUpdate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanatorium.PatientService.DTO
+{
+	public class PatientUpdate
+	{
+		public Guid Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string MiddleName { get; set; }
+		public DateOnly BirthDate { get; set; }
+		public string PhoneNumber { get; set; }
+	}
+}
