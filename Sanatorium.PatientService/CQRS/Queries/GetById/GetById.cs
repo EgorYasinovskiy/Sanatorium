4f9cda94b06ffbc80601b51def735c3463562f67
Egor Yasinovskiy <yasinovskiy.egor1@gmail.com>
2023-04-25 20:06:16 +0300

    Реализовал запрос на чтение опредленного пациента

---
+++ Sanatorium.PatientService/CQRS/Queries/GetById/GetById.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+using Sanatorium.PatientService.DataModel;
+
+namespace Sanatorium.PatientService.CQRS.Queries.GetById
+{
+	public class GetById : IRequest<Patient>
+	{
+		public Guid Id { get; set; }
+	}
+}
---
+++ Sanatorium.PatientService/CQRS/Queries/GetById/GetByIdHandler.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+
+using MediatR;
+
+using Sanatorium.PatientService.CQRS.Commands;
+using Sanatorium.PatientService.DataModel;
+
+namespace Sanatorium.PatientService.CQRS.Queries.GetById
+{
+	public class GetByIdHandler : RequestHandlerBase, IRequestHandler<GetById, Patient>
+	{
+		public GetByIdHandler(IPatientRepository patientRepository, IMapper mapper) : base(patientRepository, mapper)
+		{
+		}
+
+		public async Task<Patient> Handle(GetById request, CancellationToken cancellationToken)
+		{
+			var pacient = await _patientRepository.GetById(request.Id, cancellationToken);
+			return pacient;
+		}
+	}
+}
