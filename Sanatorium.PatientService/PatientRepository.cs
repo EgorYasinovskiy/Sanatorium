98ed2bfc0416e3184fd2552411fe59e385623337
Egor Yasinovskiy <yasinovskiy.egor1@gmail.com>
2023-04-29 17:10:03 +0300

    Реализовал класс контекста БД

---
+++ Sanatorium.PatientService/PatientRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using Sanatorium.PatientService.DataModel;
+using Sanatorium.PatientService.Interfaces;
+
+namespace Sanatorium.PatientService
+{
+	public class PatientRepository : IPatientRepository
+	{
+		private readonly IPatientDbContext _dbContext;
+
+		public PatientRepository(IPatientDbContext dbContext) =>
+			_dbContext = dbContext;
+
+		public async Task Create(Patient patient, CancellationToken cancellationToken)
+		{
+			await _dbContext.Patients.AddAsync(patient, cancellationToken);
+		}
+		public async Task DeleteById(Guid id, CancellationToken cancellationToken)
+		{
+			var patient = await _dbContext.Patients.FirstOrDefaultAsync(x => x.Id == id);
+			if (patient == null)
+				throw new ArgumentException($"Patient with Id {id} was not found");
+			_dbContext.Patients.Remove(patient);
+		}
+
+		public async Task<IEnumerable<Patient>> GetAll(CancellationToken cancellationToken)
+		{
+			return await _dbContext.Patients.ToListAsync(cancellationToken);
+		}
+
+		public async Task<Patient> GetById(Guid id, CancellationToken cancellationToken)
+		{
+			var patient = await _dbContext.Patients.FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+			if(patient == null)
+				throw new ArgumentException($"Patient with Id {id} was not found");
+
+			return patient;
+		}
+
+		public async Task SaveChanges(CancellationToken cancellationToken)
+		{
+			await _dbContext.SaveChangesAsync(cancellationToken);
+		}
+
+		public async Task Update(Patient patient, CancellationToken cancellationToken)
+		{
+			await Task.Run(() => _dbContext.Patients.Update(patient));
+		}
+	}
+}
