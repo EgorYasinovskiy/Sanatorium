b7e77b530e6f90c6b16ddc2bf0ddfd24b3f20aaf
Egor Yasinovskiy <yasinovskiy.egor1@gmail.com>
2023-04-29 16:28:57 +0300

    переименовал профиль автомаппера, добавил зависимости DI

--- Sanatorium.PatientService/Program.cs
+++ Sanatorium.PatientService/Program.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Reflection;
+
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+
+using Sanatorium.PatientService.Mappings;
 
 namespace Sanatorium.PatientService
 {
@@ -10,8 +14,19 @@ namespace Sanatorium.PatientService
 			var builder = WebApplication.CreateBuilder(args);
 
 			// Add services to the container.
-			
+
+			builder.Services.AddAutoMapper(cfg =>
+			{
+				cfg.AddProfile(new AssemblyMappingsProfile(Assembly.GetExecutingAssembly()));
+			});
+
+			builder.Services.AddMediatR(cfg =>
+			{
+				cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
+			});
+
 			builder.Services.AddControllers();
+
 			var app = builder.Build();
 
 			// Configure the HTTP request pipeline.
