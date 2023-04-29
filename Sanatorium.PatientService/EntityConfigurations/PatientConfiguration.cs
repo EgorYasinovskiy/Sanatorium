9ec0f2a66457756f201e9201307f924e28a9f7f4
Egor Yasinovskiy <yasinovskiy.egor1@gmail.com>
2023-04-29 16:45:20 +0300

    Заменил аттрибуты на конфигурацию модели, добавил контекста базы данных

---
+++ Sanatorium.PatientService/EntityConfigurations/PatientConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using Sanatorium.PatientService.DataModel;
+
+namespace Sanatorium.PatientService.EntityConfigurations
+{
+	public class PatientConfiguration : IEntityTypeConfiguration<Patient>
+	{
+		public void Configure(EntityTypeBuilder<Patient> builder)
+		{
+			builder.HasKey(x => x.Id);
+			builder.HasIndex(x => x.Id).IsUnique();
+			builder.Property(x => x.FirstName).HasMaxLength(64).IsRequired();
+			builder.Property(x => x.LastName).HasMaxLength(64).IsRequired();
+			builder.Property(x => x.MiddleName).HasMaxLength(64).IsRequired();
+			builder.Property(x => x.PhoneNumber).HasMaxLength(16).IsRequired();
+			builder.Property(x => x.Discharged).IsRequired().HasDefaultValue(true);
+		}
+	}
+}
---
+++ Sanatorium.PatientService/PatientDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using Sanatorium.PatientService.DataModel;
+using Sanatorium.PatientService.Interfaces;
+
+namespace Sanatorium.PatientService
+{
+	public class PatientDbContext : DbContext, IPatientDbContext
+	{
+		public DbSet<Patient> Patients { get; set; }
+
+		public PatientDbContext(DbContextOptions<PatientDbContext> options) : base(options) { }
+	}
+}
