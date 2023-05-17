71082d99cb2ed69511b40e389a584504baaff246
Egor Yasinovskiy <yasinovskiy.egor1@gmail.com>
2023-05-17 21:00:10 +0300

    Добавил два проекта для сервиса персонала, расписал класс работника

---
+++ Sanatorium.StaffService.Model/Staff.cs
@@ -0,0 +1,19 @@
+﻿namespace Sanatorium.StaffService.Model
+{
+	public class Staff
+	{
+		public Guid Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string MiddleName { get; set; }
+		public DateOnly BirthDate { get; set; }
+		public string PhoneNumber { get; set; }
+		public int CabinetNumber { get; set; }
+		public string Position { get; set; }
+		public Guid ManagerId { get; set; }
+		public Staff Manager { get; set; }
+		public int DayWork { get; set; }
+		public int DayHoliday { get; set; }
+		public DateOnly WorkStart { get; set; }
+	}
+}
