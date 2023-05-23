a47ef059b6181c950ffe74410a9ebcf08e9d9bd3
Egor Yasinovskiy <yasinovskiy.egor1@gmail.com>
2023-05-23 03:32:48 +0300

    impl base requesthandler

---
+++ Sanatorium.RoomService.BusinessLogic/CQRS/RequestHandlerBase.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+
+using Sanatorium.RoomService.BusinessLogic.Interfaces;
+
+namespace Sanatorium.RoomService.BusinessLogic.CQRS
+{
+	public class RequestHandlerBase
+	{
+		protected IRoomRepository _roomRepository;
+		protected IRoomMoveRepository _roomMoveRepository;
+		protected IBookingRepository _bookingRepository;
+		protected IMapper _mapper;
+
+		public RequestHandlerBase(IRoomRepository roomRepository, IRoomMoveRepository roomMoveRepository, IBookingRepository bookingRepository, IMapper mapper)
+		{
+			_roomRepository = roomRepository;
+			_roomMoveRepository = roomMoveRepository;
+			_bookingRepository = bookingRepository;
+			_mapper = mapper;
+		}
+	}
+}
