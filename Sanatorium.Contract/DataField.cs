313ff8acd498c9ded8575dda2771bec9aaa9d5a0
Egor Yasinovskiy <yasinovskiy.egor1@gmail.com>
2023-06-13 02:28:40 +0300

    Перенес в папку бэкэнда

--- Sanatorium.Contract/DataField.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sanatorium.Contract
-{
-	public class DataField
-	{
-		public string Name { get; set; }
-		public object Value { get; set; }
-		public DataFieldType Type { get; set; }
-	}
-}
